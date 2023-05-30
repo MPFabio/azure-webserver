897da48c2925508ee4725ea050dc1bb11dbe07d5
MPFabio <128690691+MPFabio@users.noreply.github.com>
2023-05-30 14:16:24 +0200

    Update init-script.sh

--- init-script.sh
+++ init-script.sh
@@ -1,6 +1,6 @@
 #Installing Docker
 sudo apt update -y && sudo apt upgrade -y
-sudo apt install curl
+sudo apt install curl -y
 sudo curl -fsSL https://get.docker.com -o get-docker.sh 
 sudo sh get-docker.sh
 
