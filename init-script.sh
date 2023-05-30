73c34fec9a0e2caa5b6016a1d62ad7eb5af9b2ac
MPFabio <128690691+MPFabio@users.noreply.github.com>
2023-05-30 12:19:07 +0200

    Update init-script.sh

--- init-script.sh
+++ init-script.sh
@@ -3,15 +3,9 @@
 #Installing Docker
 sudo apt update && sudo apt upgrade -y
 sudo apt insall curl -y
-curl -fsSL https://get.docker.com -o install-docker.sh
-sh install-docker.sh --dry-run
-sudo sh install-docker.sh
-
+sudo curl -fsSL https://get.docker.com -o get-docker.sh 
+sudo sh get-docker.sh
 
 
 #Creating container
-sudo docker pull fabiomp/docker-nginx
-sudo usermod -aG docker $USER
-sudo systemctl enable docker
-sudo systemctl restart docker
 sudo docker run -d -p 8080:80 --name docker-nginx fabiomp/docker-nginx:latest
