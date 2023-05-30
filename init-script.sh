13b234cf3c1f228ef3e4994812d8bcebe9e46c1a
MPFabio <128690691+MPFabio@users.noreply.github.com>
2023-05-30 10:37:49 +0200

    Update init-script.sh

--- init-script.sh
+++ init-script.sh
@@ -14,4 +14,4 @@ sudo docker pull fabiomp/docker-nginx
 sudo usermod -aG docker $USER
 sudo systemctl enable docker
 sudo systemctl restart docker
-sudo docker run -d -p 8080:80 --name docker-nginx -80:80 nginx:latest
+sudo docker run -d -p 8080:80 --name docker-nginx fabiomp/docker-nginx:latest
