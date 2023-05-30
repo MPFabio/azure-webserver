538dd7fdabfd812eb6447b3b97bf234629d9dd71
MPFabio <128690691+MPFabio@users.noreply.github.com>
2023-05-30 08:39:54 +0200

    Update init-script.sh

--- init-script.sh
+++ init-script.sh
@@ -1,9 +1,18 @@
-#!/bin/bash
+!/bin/bash
 
 #Installing Docker
-...
-...
+sudo apt update
+sudo apt upgrade -y
+sudo apt insall curl -y
+curl -fsSL https://get.docker.com -o install-docker.sh
+sh install-docker.sh --dry-run
+sudo sh install-docker.sh
+
+
 
 #Creating container
-...
-...
+sudo wget https://github.com/https://github.com/MPFabio/azure-webserver.git
+sudo usermod -a -G docker $USER
+sudo systemctl enable docker
+sudo systemctl restart docker
+sudo docker run --name docker-nginx -p 8080:80 nginx:latest
